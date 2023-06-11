7be1e405d3189d3d3e81266a33425fe4aa5cdf34
AnnLP2 <ann.vitalina@gmail.com>
2023-06-11 23:46:45 +0300

    added breakpoint's all styles

--- js/index.js
+++ js/index.js
@@ -1,1 +1,23 @@
 "use strict";
+
+(function () {
+    const burger = document.querySelector(".menu__burger");
+    if (!burger) return;
+    const menu = document.querySelector(".menu__list");
+    const overlay = document.querySelector(".menu__overlay");
+    const body = document.querySelector("body");
+
+    burger.addEventListener("click", function () {
+        menu.classList.toggle("show");
+        burger.classList.toggle("show");
+        overlay.classList.toggle("show");
+        body.classList.toggle("stop-scroll");
+    });
+
+    overlay.addEventListener("click", function () {
+        menu.classList.remove("show");
+        burger.classList.remove("show");
+        overlay.classList.remove("show");
+        body.classList.remove("stop-scroll");
+    });
+})();
